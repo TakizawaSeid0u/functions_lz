9784e7239ff824d1b68d55e3947c212179eea5f5
takizawaseid0u <takizawaseidou@tutamail.com>
2024-12-03 17:05:54 +0300

    терь готово

--- temperature.py
+++ temperature.py
@@ -2,6 +2,6 @@ print('введите число') #выводим текст на поле
 m = input() #считываем число
 v = int(0)
 def temperature(n, c):
-    c = (int(n)-32)*5/9
+    c = (float(n)-32)*5/9
     return(c)
 print(temperature(m, v))
