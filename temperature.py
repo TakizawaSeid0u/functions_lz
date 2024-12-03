c5471e0ba676d29cf1bd075199e74d9b361ce00d
takizawaseid0u <takizawaseidou@tutamail.com>
2024-12-03 17:08:56 +0300

    АЫВАЫВАЫВА

--- temperature.py
+++ temperature.py
@@ -2,6 +2,6 @@ print('введите число') #выводим текст на поле
 m = input() #считываем число
 v = int(0)
 def temperature(n, c):
-    c = (float(n)-32)*5/9
+    c = (float(n)*9/5)+32
     return(c)
-print(temperature(m, v))
+print("температура по Фаренгейту", temperature(m, v))
